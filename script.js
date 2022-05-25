27b78e3a7ba55de9951a4b25d6631297851d57fc
Rossyrose <102945493+Rossyrose@users.noreply.github.com>
2022-05-25 08:35:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".menu-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    }
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    }
+}};
